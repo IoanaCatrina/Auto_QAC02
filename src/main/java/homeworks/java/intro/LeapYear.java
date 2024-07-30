9e00666536a356597678b1c8d1f0960e26c434a7
Ioana Catrina <ioana.catrina@outlook.com>
2024-07-30 18:56:10 +0300

    Update LeapYear

--- src/main/java/homeworks/java/intro/LeapYear.java
+++ src/main/java/homeworks/java/intro/LeapYear.java
@@ -7,9 +7,9 @@ public class LeapYear {
             System.out.println("The year should be between 1900 - 2016!");
         } else {
             if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
-                System.out.println("Year " + year + " has 29 days");
+                System.out.println("February " + year + " has 29 days");
             } else {
-                System.out.println("Year " + year + " has 28 days");
+                System.out.println("February " + year + " has 28 days");
             }
         }
     }
