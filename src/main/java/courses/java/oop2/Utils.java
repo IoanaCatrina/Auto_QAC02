f11c3c5beb7fc757875ddd6a10b0c6eed6f3162a
Ioana Catrina <ioana.catrina@outlook.com>
2024-08-14 12:25:51 +0300

    Java OOP2 - Encapsulation, Lombok, Static methods, Inheritance, Overriding and Polymorphism

---
+++ src/main/java/courses/java/oop2/Utils.java
@@ -0,0 +1,12 @@
+package courses.java.oop2;
+
+public class Utils {
+    //    Static - needs to be called from the class
+    public static void printCircleStatic(Circle c) {
+        System.out.println("Color: " + c.getColor());
+        System.out.println("Vertices count: " + c.getVertices());
+        System.out.println("Radius " + c.getRadius());
+        System.out.println("--------------");
+    }
+
+}
