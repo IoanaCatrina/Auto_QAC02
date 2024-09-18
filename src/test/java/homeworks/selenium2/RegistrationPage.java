44278fa17307637305ee75fa1de6703ef28d4b46
Ioana Catrina <ioana.catrina@outlook.com>
2024-09-18 13:22:13 +0300

    negativeLogin Tests and registration Tests

--- src/test/java/homeworks/selenium2/RegistrationPage.java
+++ src/test/java/homeworks/selenium2/RegistrationPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import pages.BasePage;
 
-import java.sql.SQLOutput;
+
 
 public class RegistrationPage extends BasePage {
     @FindBy(xpath = "//h1[contains(text(), 'Sign up')]")
