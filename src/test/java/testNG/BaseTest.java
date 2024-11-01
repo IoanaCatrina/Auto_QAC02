3903b1a9ebd3df4a880ce27e4d651f3a45ec38ab
Ioana Catrina <ioana.catrina@outlook.com>
2024-11-01 13:31:35 +0200

    Test Execution and Reporting

---
+++ src/test/java/testNG/BaseTest.java
@@ -0,0 +1,23 @@
+package testNG;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    public static ExtentReports extent;
+
+    @BeforeSuite
+    public void setUp() {
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("reports/report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+    }
+
+    @AfterSuite
+    public void clearUp() {
+        extent.flush();
+    }
+}
